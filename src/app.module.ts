6c8d9628d32b5a62134b0375a60f6d28c10d8f04
Nguyen Duc Hoa <nguyenduchoa0130@gmail.com>
2024-01-04 21:38:44 +0700

    Integrate logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Logtail } from '@logtail/node';
+import { LogtailTransport } from '@logtail/winston';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DistrictsModule } from './modules/districts/districts.module';
-import { SurfaceTypeModule } from './modules/surface-type/surface-type.module';
-import { SpaceTypeModule } from './modules/space-type/space-type.module';
-import { SpaceFormatsModule } from './modules/space-format/space-formats.module';
-import { ReportFormatsModule } from './modules/report-format/report-formats.module';
-import { CompanyModule } from './modules/company/company.module';
-import { ReportModule } from './modules/report/report.module';
-import { UsersModule } from './modules/users/users.module';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
 import { AuthModule } from './modules/auth/auth.module';
-import { SurfacesModule } from './modules/surfaces/surfaces.module';
+import { CompanyModule } from './modules/company/company.module';
+import { DistrictsModule } from './modules/districts/districts.module';
+import { ReportFormatsModule } from './modules/report-format/report-formats.module';
+import { ReportModule } from './modules/report/report.module';
+import { SpaceFormatsModule } from './modules/space-format/space-formats.module';
+import { SpaceTypeModule } from './modules/space-type/space-type.module';
 import { SpacesModule } from './modules/spaces/spaces.module';
+import { SurfaceTypeModule } from './modules/surface-type/surface-type.module';
+import { SurfacesModule } from './modules/surfaces/surfaces.module';
+import { UsersModule } from './modules/users/users.module';
+import { LoggerMiddleware } from './shared/middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -34,8 +43,45 @@ import { SpacesModule } from './modules/spaces/spaces.module';
     AuthModule,
     SurfacesModule,
     SpacesModule,
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('Ads Management', {
+              colors: true,
+              prettyPrint: true,
+            }),
+          ),
+        }),
+        new winston.transports.DailyRotateFile({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('Ads Management', {
+              colors: true,
+              prettyPrint: true,
+            }),
+          ),
+          dirname: 'logs',
+          filename: 'application-%DATE%.log',
+          handleExceptions: true,
+          json: false,
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '2d',
+        }),
+        new LogtailTransport(new Logtail(process.env.LOGTAIL_TOKEN)),
+      ],
+      exitOnError: false,
+    }),
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   SwaggerModule.setup('docs', app, document);
 
   await app.listen(3001);
